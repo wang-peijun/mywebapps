a4399e0c339958c4dd9564b09f34d155957596c1
adm <w17539@outlook.com>
2018-09-08 09:13:46 +0800

    完善代码

--- mywebapp/settings.py
+++ mywebapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9#esdf#k#_&m=ad3!@q^y!c74i9ohbe1y(9mu3mp=7!e)7%zhh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
